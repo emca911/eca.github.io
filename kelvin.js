9c8d92bff148400eee719dae77d918382fa92297
emca911 <119214233+emca911@users.noreply.github.com>
2023-02-28 22:38:33 -0500

    Update kelvin.js

--- kelvin.js
+++ kelvin.js
@@ -1,26 +1,48 @@
-document.getElementById("cInput").oninput = function() {convertKToF()};
-document.getElementById("fInput").oninput = function() {convertFToK()};
-
-function convertKToF() {
-  var k = document.querySelector("#kInput").value;
-  var f = 1.8 * (k - 273) + 32;
-
-  k = parseInt(k);
-  f = parseInt(f);
-
-  document.querySelector("#result").innerHTML = ("Result: ") + f + (" \u2109");
+function getElm(id){
+  return document.getElementById(id);
 }
 
-document.querySelector("#submit").onclick = convertKToF;
+function readValue(id){
+  return getElm(id).value;
+}
 
-function convertFToK() {
-  var f = document.querySelector("#fInput").value;
+function updateHtml(text,id){
+  getElm(id).innerHTML  = text;
+}
+
+
+function convertKToF(k) {
+  var f = 1.8 * (k - 273) + 322;
+  return f;
+}
+
+function convertFToK(f) {
   var k = (f + 459.67) * 5 / 9;
-
-  k = parseInt(k);
-  f = parseInt(f);
-
-  document.querySelector("#result").innerHTML = ("Result: ") + k + (" \u2103");
+  return k;
 }
 
-document.querySelector("#submit").onclick = convertFToK;
+function sequnce(){
+  var val =  readValue(activeId);
+  var convertedVal = activeFn(val);
+  updateHtml("Result: "+ convertedVal + activeSuffix,'result')
+}
+
+var activeFn = convertKToF;
+var activeId = 'kInput';
+var activeSuffix = "℉"
+
+getElm('cInput').onfocus = function(){
+  activeFn = convertKToF;
+  activeId = 'kInput';
+  activeSuffix = "℉"
+}
+
+getElm('fInput').onfocus  = function(){
+  activeFn = convertKToF;
+  activeId = 'fInput';
+  activeSuffix = "K"
+}
+
+getElm("kInput").oninput = sequnce;
+getElm("fInput").oninput = sequnce;
+getElm("submit").oninput = sequnce;
